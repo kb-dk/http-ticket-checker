f94550220bdf560c3818432bd21f72ecce629553
Adam Tulinius <aft@statsbiblioteket.dk>
2013-04-16 13:17:29 +0200

    separate out ticket-related functions

---
+++ src/http_ticket_checker_clj/tickets.clj
@@ -0,0 +1,65 @@
+(ns http-ticket-checker-clj.tickets
+  (:use http-ticket-checker-clj.configuration)
+  (:require [clojurewerkz.spyglass.client :as m])
+  (:require [clojure.data.json :as json]))
+
+
+(def ticket-store-atom
+  (atom nil))
+
+(defn create-ticket-store []
+  (m/bin-connection "alhena:11211"))
+
+(defn get-ticket-store []
+  (deref ticket-store-atom))
+
+(defn set-ticket-store [ticket-store]
+  (swap! ticket-store-atom
+    (fn [_] ticket-store)))
+
+
+(defn get-ticket [ticket_id]
+  (m/get (get-ticket-store) ticket_id))
+
+(defn parse-ticket [raw_ticket]
+  (if raw_ticket
+    (let [ticket (json/read-str raw_ticket)]
+      (let [resource_ids (ticket "resources")
+            presentationType (ticket "type")
+            userIdentifier (ticket "userIdentifier")]
+        (if (and resource_ids type userIdentifier)
+          {:resource_ids resource_ids :presentationType presentationType :userIdentifier userIdentifier}
+          nil)))
+    nil))
+
+
+(defn get-resource-id [resource]
+  (first
+    (clojure.string/split
+      (last
+        (clojure.string/split
+          resource
+          #"/"))
+      #"\.")))
+
+
+(defn shorten-resource-id [resource_id]
+  (last
+    (clojure.string/split resource_id #":")))
+
+
+(defn valid-ticket? [resource ticket_id]
+  (let [ticket (get-ticket ticket_id)
+        resource_id (get-resource-id resource)]
+    (if (and ticket resource_id)
+      (let [parsed_ticket (parse-ticket ticket)]
+        (if parsed_ticket
+          (and
+            (= ((get-config) :presentation_type) (parsed_ticket :presentationType))
+            (not
+              (not
+                (some #{resource_id} (list
+                                       (last
+                                         (map shorten-resource-id (parsed_ticket :resource_ids))))))))
+          false))
+      false)))
