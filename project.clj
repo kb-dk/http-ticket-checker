dc50542afd0fc30edb8942fb0cc832b8783e42a2
Adam Tulinius <aft@statsbiblioteket.dk>
2013-04-17 23:27:45 +0200

    upgrade to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject http-ticket-checker-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.2"]
                  [compojure "1.1.5"]
                  [clojurewerkz/spyglass "1.0.2"]]
