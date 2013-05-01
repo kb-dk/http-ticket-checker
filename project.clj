4d2a158af5fea2507d89964c5b72140cabbae366
Adam Tulinius <aft@statsbiblioteket.dk>
2013-05-01 12:55:31 +0200

    change namespace to dk.statsbiblioteket.medieplatform, and add missing bits for creating pom-files

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http-ticket-checker "0.1.0-SNAPSHOT"
+(defproject dk.statsbiblioteket.medieplatform/http-ticket-checker "1.0"
   :description "HTTP-ticket-checker is a web-application that serves resources iff the supplied ticket is valid.
   Tickets are retrieved from memcached, deserialized from json, and compared with the requested resource,
   client ip-address, and the configured presentation-type."
@@ -13,6 +13,11 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [compojure "1.1.5"]
                  [clojurewerkz/spyglass "1.0.2"]]
+  :scm {:url "git@github.com:statsbiblioteket/http-ticket-checker.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Adam Tulinius"]
+                              [:email "aft@statsbiblioteket.dk"]
+                              [:timezone "+1"]]]
   :plugins [[lein-ring "0.8.2"]
             [lein-marginalia "0.7.1"]]
   :ring {:handler http-ticket-checker.handler/app
